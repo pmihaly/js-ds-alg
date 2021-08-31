a002f1bc202fd1b5a0f7e6856a800cfa8e1e7d38
pmihaly <misi@pappmihaly.com>
2021-08-31 22:38:23 +0200

    test: :white_check_mark: üres objektum teszt

--- 03-employee-salary.spec.js
+++ 03-employee-salary.spec.js
@@ -4,5 +4,8 @@ describe("calculate company expenses", () => {
   it("should correctly calculate all expenses", () => {
     expect(calculateExpenses(sampleCompany)).toBe(369000);
   });
-});
 
+  it("should calculate and expense of 0 when given empty object", () => {
+    expect(calculateExpenses({})).toBe(0);
+  });
+});
